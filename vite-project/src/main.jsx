452f0fa967b211590e388ff306a6aac5fa90d358
ikefix <omekeikechukwu877@gmail.com>
2026-03-30 06:29:51 -0700

    frontend code

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -8,13 +8,11 @@ import { AuthProvider } from "./contexts/AuthContext";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-<<<<<<< HEAD
+
       <AuthProvider>
         <App />
       </AuthProvider>
-=======
       <App />
->>>>>>> e8bbc507096c7552c908a45da85ab862a6a1000a
     </BrowserRouter>
   </StrictMode>
 );
